401067534423edb747befb559cd737b1b8844bd9
Swayam <swayamkumar226@gmail.com>
2025-11-02 11:54:28 +0530

    added meme text placeholder

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,31 @@
 export default function Hero() {
   return (
     <main className="w-full h-full flex justify-between flex-row mt-10 items-center gap-36">
-      <section className="pt-20 h-[500px] flex-1 flex flex-col justify-center items-center bg-gradient-to-r from-violet-600 to-green-400 pl-10 pr-10 rounded-3xl hover:scale-103 gap-25 transition-transform duration-300 ml-10 hover:shadow-2xl shadow-xl">
-        {/* this is gonna be the straight section where everything will lie*/}
-        <div className="w-full h-40 flex flex-col justify-around items-center gap-10">
-          <div className="h-auto w-full text-lg text-purple-200 font-semibold">
-            <label htmlFor="">
+      {/* Left section — form */}
+      <section className="pt-10 h-[500px] flex-1 flex flex-col justify-center items-center bg-gradient-to-r from-violet-600 to-green-400 px-10 rounded-3xl hover:scale-103 gap-5 transition-transform duration-300 ml-10 hover:shadow-2xl shadow-xl">
+        <div className="text-4xl text-center w-full pb-5">
+          <span className="text-purple-200 font-bold">
+            Best you can think of
+          </span>
+        </div>
+        <div className="w-full flex flex-col justify-around items-center gap-6">
+          <div className="w-full text-lg text-purple-200 font-semibold">
+            <label>
               Top Text
               <input
-                className="rounded-md font-semibold p-2 mt-1 w-full h-12 border-gray-400 block border-2 bg-purple-300 text-black
-                shadow-md"
+                className="rounded-md font-semibold p-2 mt-1 w-full h-12 border-gray-400 border-2 bg-purple-300 text-black shadow-md"
                 type="text"
                 name="top-text"
                 id="top-text"
               />
             </label>
           </div>
-          <div className="h-auto w-full text-purple-200 font-semibold text-lg">
-            <label htmlFor="">
+
+          <div className="w-full text-lg text-purple-200 font-semibold">
+            <label>
               Bottom Text
               <input
-                className="rounded-md font-semibold p-2 mt-1 w-full h-12 border-gray-400 block border-2 bg-purple-300 text-black shadow-md"
+                className="rounded-md font-semibold p-2 mt-1 w-full h-12 border-gray-400 border-2 bg-purple-300 text-black shadow-md"
                 type="text"
                 name="bottom-text"
                 id="bottom-text"
@@ -28,7 +33,8 @@ export default function Hero() {
             </label>
           </div>
         </div>
-        <div className="w-full font-semibold h-30">
+
+        <div className="w-full font-semibold mt-4">
           <button className="h-15 w-full bg-gradient-to-r from-violet-700 to-green-500 rounded-2xl text-xl text-purple-200 shadow-md hover:shadow-3xl hover:scale-102 active:scale-95 transform transition-transform duration-300">
             Get the damn meme!!
           </button>
@@ -36,15 +42,20 @@ export default function Hero() {
       </section>
 
       <section className="flex-1 h-[500px] flex items-center justify-center bg-gradient-to-r from-violet-600 to-green-400 p-10 rounded-3xl hover:scale-103 transition-transform duration-300 hover:shadow-2xl shadow-xl mr-10">
-        <div className="w-full h-full flex flex-col  justify-center p-6 rounded-2xl">
-          <span className="pb-3 text-purple-200 text-lg font-semibold">
-            Your Generated Meme:
+        <div className="relative w-full h-full rounded-2xl overflow-hidden items-center justify-center flex flex-col">
+          <span className="absolute top-15 left-1/2 -translate-x-1/2 text-center font-[impact,sans-serif] text-2xl uppercase text-white tracking-[1px] meme-text-shadow">
+            Your meme text here
           </span>
+
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEc9k2kuUxms3gQHFAHMBLUTjMSSGC-Zh2Ww&s"
             alt="generated-meme"
-            className="object-cover w-full h-full rounded-xl"
+            className="object-cover w-[85%] h-[85%] rounded-xl"
           />
+
+          <span className="absolute bottom-15 left-1/2 -translate-x-1/2 text-center font-[impact,sans-serif] text-2xl uppercase text-white tracking-[1px] meme-text-shadow">
+            Your meme text here
+          </span>
         </div>
       </section>
     </main>
